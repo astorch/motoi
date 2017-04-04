7c79d84eaba746da0ce0df97a07189617dce1201
Arian Storch <arian.storch@bflow.org>
2017-04-04 10:51:46 +0200

    Added ProductAssemblyInfo.cs (global)

---
+++ src/ProductAssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("debug")]
+#else 
+[assembly: AssemblyConfiguration("release")]
+#endif
+
+[assembly: AssemblyCompany("motoi.org")]
+[assembly: AssemblyProduct("motoi .NET RCP")]
+[assembly: AssemblyCopyright("Copyright ©  2017 motoi.org")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+namespace motoi.platform.ui.properties {
+}
