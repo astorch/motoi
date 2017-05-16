f09697e91597674aa6f3b607a69ff4a074f4eb26
Arian Storch <arian.storch@bflow.org>
2017-05-16 20:23:10 +0200

    Linked PlatformProductAssemblyInfo.cs in platform plug-ins

--- src/plug-ins/motoi.platform.resources/properties/AssemblyInfo.cs
+++ src/plug-ins/motoi.platform.resources/properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("motoi.platform.application")]
-[assembly: AssemblyDescription("Provides the platform application API")]
+[assembly: AssemblyTitle("motoi.platform.resources")]
+[assembly: AssemblyDescription("Provides the API of the platform resources")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -14,4 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1f1d5cbb-7e39-45f1-819c-5d0c3eee5aa4")]
+[assembly: Guid("16429d8b-cdaf-4b0f-83a9-59c1a6f5571d")]
