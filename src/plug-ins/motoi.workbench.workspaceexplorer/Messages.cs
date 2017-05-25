060ac2f4c7cd1ae4f35d5bad639cde3d363e479e
Arian Storch <arian.storch@bflow.org>
2017-05-25 21:26:13 +0200

    Added localized data view label

---
+++ src/plug-ins/motoi.workbench.workspaceexplorer/Messages.cs
@@ -0,0 +1,16 @@
+﻿using motoi.platform.nls;
+
+namespace motoi.workbench.workspaceexplorer {
+    // ReSharper disable UnassignedReadonlyField
+    // ReSharper disable InconsistentNaming
+    #pragma warning disable 1591
+
+    /// <summary> Provides the messages of the plug-in. </summary>
+    public class Messages : NLS<Messages> {
+        static Messages() {
+            LoadMessages();
+        }
+
+        public static readonly string Extensions_WorkspaceExploreryView_Label;
+    }
+}
