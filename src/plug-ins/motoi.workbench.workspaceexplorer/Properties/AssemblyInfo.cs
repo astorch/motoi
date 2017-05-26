1a4921b34b76de8602601a9910f0ca9a4f0d3ae9
Arian Storch <arian.storch@bflow.org>
2017-05-26 21:08:40 +0200

    Corrected assembly title

--- src/plug-ins/motoi.workbench.workspaceexplorer/Properties/AssemblyInfo.cs
+++ src/plug-ins/motoi.workbench.workspaceexplorer/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("motoi.ui.workspaceexplorer")]
+[assembly: AssemblyTitle("motoi.workbench.workspaceexplorer")]
 [assembly: AssemblyDescription("Implements a workbench workspace explorer")]
 [assembly: AssemblyCulture("")]
 
