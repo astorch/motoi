61e315005f1ce842d6871b2d5ca12d7e333b7ed1
Arian Storch <arian.storch@bflow.org>
2017-06-02 14:23:13 +0200

    Modified source documentation style

--- src/plug-ins/motoi.platform.ui/shells/EMessageDialogResult.cs
+++ src/plug-ins/motoi.platform.ui/shells/EMessageDialogResult.cs
@@ -1,34 +1,25 @@
 using Xcite.Csharp.lang;
 
 namespace motoi.platform.ui.shells {
-    /// <summary>
-    /// Defines kinds of message dialog results.
-    /// </summary>
+    /// <summary> Defines kinds of message dialog results. </summary>
     public class EMessageDialogResult : XEnum<EMessageDialogResult> {
-        /// <summary>
-        /// Indicates the ok result.
-        /// </summary>
+        /// <summary> Indicates the ok result. </summary>
         public static readonly EMessageDialogResult Ok = new EMessageDialogResult("Ok");
 
-        /// <summary>
-        /// Indicates the cancel result.
-        /// </summary>
+        /// <summary> Indicates the cancel result. </summary>
         public static readonly EMessageDialogResult Cancel = new EMessageDialogResult("Cancel");
 
-        /// <summary>
-        /// Indicates the yes result.
-        /// </summary>
+        /// <summary> Indicates the yes result. </summary>
         public static readonly EMessageDialogResult Yes = new EMessageDialogResult("Yes");
 
-        /// <summary>
-        /// Indicates the no result.
-        /// </summary>
+        /// <summary> Indicates the no result. </summary>
         public static readonly EMessageDialogResult No = new EMessageDialogResult("No");
 
-        /// <summary>
-        /// Indicates a none result.
-        /// </summary>
+        /// <summary> Indicates a none result. </summary>
         public static readonly EMessageDialogResult None = new EMessageDialogResult("None");
+
+        /// <summary> Indicates a close result. </summary>
+        public static readonly EMessageDialogResult Close = new EMessageDialogResult("Close");
 
         /// <summary>
         /// Protected constructor.
