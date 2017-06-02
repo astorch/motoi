795e5b5f02cfec56c06fbaf8fcb6ec53e9f09d31
Arian Storch <arian.storch@bflow.org>
2017-06-02 14:24:12 +0200

    Introducing IExceptionDialog as shell

--- src/plug-ins/motoi.platform.ui/shells/IExceptionDialog.cs
+++ src/plug-ins/motoi.platform.ui/shells/IExceptionDialog.cs
@@ -1,13 +1,18 @@
-﻿namespace motoi.platform.ui.shells {
+﻿using System;
+using motoi.platform.ui.bindings;
+
+namespace motoi.platform.ui.shells {
     /// <summary> Defines an exception dialog. </summary>
     public interface IExceptionDialog : IMessageDialog {
-        
+        /// <summary> Returns the exception to show or does set it. </summary>
+        Exception Exception { get; set; }
     }
 
     /// <summary>
     /// Provides the property meta data of <see cref="IExceptionDialog"/> that is used by data binding operations.
     /// </summary>
     public class PExceptionDialog : PMessageDialogControl<IExceptionDialog> {
-        
+        /// <summary> Exception property meta data </summary>
+        public static readonly IBindableProperty<Exception> ExceptionProperty = CreatePropertyInfo(nameof(IExceptionDialog.Exception), (Exception)null);
     }
 }
