b01de10427beb7c4ad53fb376c36ad215234c0d0
Arian Storch <arian.storch@bflow.org>
2017-06-03 16:34:49 +0200

    Introducing selection changed event

---
+++ src/plug-ins/motoi.workbench/events/SelectionChangedEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace motoi.workbench.events {
+    /// <summary> Describes a selection changed event. </summary>
+    public class SelectionChangedEvent {
+        /// <summary>
+        /// Creates a new instance with the given arguments.
+        /// </summary>
+        /// <param name="newSelection">New selection</param>
+        /// <param name="oldSelection">Old selection</param>
+        public SelectionChangedEvent(object newSelection, object oldSelection) {
+            NewSelection = newSelection;
+            OldSelection = oldSelection;
+        }
+
+        /// <summary> Returns the previously selected object. </summary>
+        public object OldSelection { get; private set; }
+
+        /// <summary> Returns the newly selected object. </summary>
+        public object NewSelection { get; private set; }
+    }
+}
