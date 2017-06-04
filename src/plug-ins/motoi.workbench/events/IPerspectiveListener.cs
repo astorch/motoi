2701abf26439f754bf0260b270e3413ee18f0ff2
Arian Storch <arian.storch@bflow.org>
2017-06-04 12:02:21 +0200

    Added OnWorkbenchPartActivated to IPerspectiveListener

--- src/plug-ins/motoi.workbench/events/IPerspectiveListener.cs
+++ src/plug-ins/motoi.workbench/events/IPerspectiveListener.cs
@@ -1,15 +1,19 @@
 ﻿using motoi.workbench.model;
 
 namespace motoi.workbench.events {
-    /// <summary>
-    /// Defines a listener of perspective events.
-    /// </summary>
+    /// <summary>  Defines a listener of perspective events. </summary>
     public interface IPerspectiveListener {
         /// <summary>
         /// Tells the instance that the given <paramref name="workbenchPart"/> has been opened.
         /// </summary>
         /// <param name="workbenchPart">Opened workbench part</param>
         void OnWorkbenchPartOpened(IWorkbenchPart workbenchPart);
+
+        /// <summary>
+        /// Tells the instance that the given <paramref name="workbenchPart"/> has been activated.
+        /// </summary>
+        /// <param name="workbenchPart">Activated workbench part</param>
+        void OnWorkbenchPartActivated(IWorkbenchPart workbenchPart);
 
         /// <summary>
         /// Tells the instance that the given <paramref name="workbenchPart"/> has been closed.
