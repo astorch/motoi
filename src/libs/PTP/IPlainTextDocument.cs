fc6e66cb3a40a7f6d8697adf04b75b7daf48708d
Arian Storch <arian.storch@evy-solutions.de>
2018-05-29 17:42:23 +0200

    Code style changes

--- src/libs/PTP/IPlainTextDocument.cs
+++ src/libs/PTP/IPlainTextDocument.cs
@@ -1,25 +1,22 @@
 using System.Collections.Generic;
 
-namespace PTP
-{
-	/// <summary>
-	/// Defines a PlainTextDocument.
-	/// </summary>
-	public interface IPlainTextDocument : IEnumerable<KeyValuePair<string, string[]>> {
-		/// <summary>
-		/// Selects the value referenced by the given xplain query. If the value doesn't 
-		/// exists null will be returned.
-		/// </summary>
+namespace PTP {
+    /// <summary> Defines a PlainTextDocument. </summary>
+    public interface IPlainTextDocument : IEnumerable<KeyValuePair<string, string[]>> {
+        /// <summary>
+        /// Selects the value referenced by the given xplain query. If the value doesn't 
+        /// exists null will be returned.
+        /// </summary>
         /// <param name="xplainQuery">Xplain query</param>
-		/// <returns>Value or null</returns>
-		string SelectValue(string xplainQuery);
-		
-		/// <summary>
-		/// Selects the valuew referenced by the given xplain query. If the values don't 
-		/// exists null will be returned.
-		/// </summary>
+        /// <returns>Value or null</returns>
+        string SelectValue(string xplainQuery);
+
+        /// <summary>
+        /// Selects the valuew referenced by the given xplain query. If the values don't 
+        /// exists null will be returned.
+        /// </summary>
         /// <param name="xplainQuery">Xplain query</param>
-		/// <returns>Values or null</returns>
-		string[] SelectValues(string xplainQuery);
-	}
+        /// <returns>Values or null</returns>
+        string[] SelectValues(string xplainQuery);
+    }
 }
