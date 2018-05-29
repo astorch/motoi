ca78f02a33969c89451e31d4376dc6f605c0368a
Arian Storch <arian.storch@evy-solutions.de>
2018-05-29 10:29:46 +0200

    Migrated motoi.moose.applicationstarter to .net 47

--- src/libs/motoi.moose.applicationstarter/EntryPoint.cs
+++ src/libs/motoi.moose.applicationstarter/EntryPoint.cs
@@ -3,16 +3,14 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading;
-using log4net;
+using System.Xml;
+using NLog;
+using NLog.Config;
 
 namespace motoi.moose.applicationstarter {
-    /// <summary>
-    /// Provides the entry point of an motoi based application.
-    /// </summary>
+    /// <summary> Provides the entry point of an motoi based application. </summary>
     public class EntryPoint {
-        /// <summary>
-        /// Implements the entry point for the execution engine.
-        /// </summary>
+        /// <summary> Implements the entry point for the execution engine. </summary>
         /// <param name="args">Execution arguments</param>
         static void Main(string[] args) {
             // Show a console window if desired
@@ -21,8 +19,8 @@ namespace motoi.moose.applicationstarter {
 
             // Configure logging
             ConfigureLog();
-            ILog log = LogManager.GetLogger(typeof(EntryPoint));
-            log.InfoFormat("Log writer initialized");
+            Logger log = LogManager.GetCurrentClassLogger(typeof(EntryPoint));
+            log.Info("Log writer initialized");
 
             string platformPluginName = "motoi.platform.application";
 
@@ -45,17 +43,18 @@ namespace motoi.moose.applicationstarter {
                 // Hide the console window
                 ConsoleManager.HideConsoleWindow();
             } catch (Exception ex) {
-                log.ErrorFormat("Error on starting plug-in '{0}'. Reason: {1}", platformPluginName, ex);
+                log.Error(ex, $"Error on starting plug-in '{platformPluginName}'");
             }
         }
 
-        /// <summary>
-        /// Configures the log4net framework.
-        /// </summary>
+        /// <summary> Configures the log4net framework. </summary>
         private static void ConfigureLog() {
-            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("motoi.moose.applicationstarter.log4net.config")) {
-                if (stream == null) throw new InvalidOperationException("Could not read log4net.config stream");
-                log4net.Config.XmlConfigurator.Configure(stream);
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("motoi.moose.applicationstarter._nlog.config")) {
+                if (stream == null) throw new InvalidOperationException("Could not read nlog.config stream");
+                using (XmlReader reader = XmlReader.Create(stream)) {
+                    XmlLoggingConfiguration config = new XmlLoggingConfiguration(reader, "nlog.config");
+                    LogManager.Configuration = config;
+                }
             }
         }
     }
